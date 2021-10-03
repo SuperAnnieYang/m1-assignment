e9af4df01c625e448fdfdbe53e312791350f8729
Jimmy Tang <superjimmyman@jimmys-mbp.lan>
2021-10-02 18:27:20 -0700

    lab assignment final lightbox

--- JS/popupbox.js
+++ JS/popupbox.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 
     $('.lightbox-toggle img').click(function(){
+        
         $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
         $('.box').fadeIn();
 
         if ($('.box').contents('img')) {
             $('.box').contents().remove('img');
         }
-
-        var img = $(this).clone();
-        $('.box').append(img);
-
+        var image = $(this).clone();
+        $('.box').append(image);
     });
 
     $('.close, .backdrop').click(function(){
